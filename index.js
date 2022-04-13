7a48d442bbbfa3e31adbc7544059879c01615ab1
Samuel Banya <sbanya@fastmail.com>
2022-04-12 20:05:31 -0400

    Adding latest changes as per assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainTag = document.querySelector("#main");
+mainTag.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = "Sam is the champion";
+
+document.body.append(newHeader);
+
